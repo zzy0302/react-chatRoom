c36b82bc43408e6cd4fecc5fea7ec6be75e9f9e8
JesBrian <JesBrian666@gmail.com>
2018-03-19 22:49:02 +0800

    引入React-Router & 尝试使用路由

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+		  <Link to='/home'>Home</Link> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+		  <Link to='/roster'>Roster</Link>
+
+          <Header />
+		  <Main />
+
       </div>
     );
   }
 }
+
+const Main = () => (
+	<div>
+		<Switch>
+			<Route path='/home' component={MainHome}/>
+			<Route path='/roster' component={MainRoster}/>
+		</Switch>
+	</div>
+)
+const MainHome = () => (
+	<div>
+		<h2>This is a home page!</h2>
+	</div>
+)
+const MainRoster = () => (
+	<div>
+		<h2>This is a roster page!</h2>
+	</div>
+)
+
+
+const Header = () => (
+	<div>
+		<Switch>
+			<Route path='/home' component={HeaderHome}/>
+			<Route path='/roster' component={HeaderRoster}/>
+		</Switch>
+	</div>
+)
+const HeaderHome = () => (
+	<div>
+		<h2>This is a home Header!</h2>
+	</div>
+)
+const HeaderRoster = () => (
+	<div>
+		<h2>This is a roster Header!</h2>
+	</div>
+)
 
 export default App;
